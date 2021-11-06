66ad4a669e1ebe463bd204d1f39984fd31212feb
ewilliams-zoot <ezekiel.williams@zootweb.com>
2021-11-05 19:40:36 -0600

    Add post endpoint

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 const express = require('express');
 const PORT = process.env.PORT || 5000;
 
-express()
-  .get('/sean', (req, res) => res.status(200).json({"message": "Hey Sean, I hope this helps your learning move faster"}))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/sean', (req, res) => {
+  return res.status(200).json({"message": "Hey Sean, I hope this helps your learning move faster"});
+});
+
+app.post('/sean', (req, res) => {
+  if (req.is("application/x-www-form-urlencoded")) {
+    const respMsg = `You have submitted the following named fields: ${Object.keys(req.body).join(', ')}`;
+    return res.status(200).json({"message": respMsg});
+  }
+
+  return res.status(400).json({"message": "This endpoint is for 'application/x-www-form-urlencoded' content types only."});
+});
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
