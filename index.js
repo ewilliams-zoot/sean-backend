345948829ea32c9f29de1c112a8f496bcaae8e97
ewilliams-zoot <ezekiel.williams@zootweb.com>
2021-11-05 19:13:14 -0600

    Reduce project to simple HTTP server

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const PORT = process.env.PORT || 5000;
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .get('/sean', (req, res) => res.status(200).json({"message": "Hey Sean, I hope this helps your learning move faster"}))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
